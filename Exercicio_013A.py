f2f9bc47c1e1441c9c151d6cd388d96226b51a12
Eduarda Alves Pontel <109531518+eduardapontel@users.noreply.github.com>
2022-09-09 08:54:17 -0300

    Add files via upload

--- Exercicio_013A.py
+++ Exercicio_013A.py
@@ -4,7 +4,21 @@ print('{:^35}'.format('REAJUSTE DE SALÁRIO'))
 print(bordas)
 print()
 
-s = float(input('Digite o salário do funcionário: R$'))
-a = int(input('Digite a porcentagem de aumento: '))
+s = ''
+
+while type(s) != float:
+    try:
+        s = float(input('Digite o salário do funcionário: R$ '))
+    except:
+        print('Por favor digite um salário válido!')
+
+a = ''
+
+while type(a) != float:
+    try:
+        a = float(input('Digite a porcentagem de aumento: '))
+    except:
+        print('Por favor digite uma porcentagem válida!')
+
 print(f'Com o aumento de {a}% o valor do aumento será de R${s*(a/100):.2f} e o funcionário que ganhava R${s:.2f} vai '
       f'passar a ganhar R${s+s*(a/100):.2f}.')
